b8b01424d01cf625e0d0c3d28d42295fab508671
NERO-MK <minkhamy1@email.com>
2025-06-09 14:28:32 +0630

    Fix canvas height and close tag

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function GamePage() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "/game/main.js"; // This points to your built game file
+    script.src = "/game/main.js";
     script.async = true;
     document.body.appendChild(script);
 
@@ -16,7 +16,7 @@ export default function GamePage() {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Farmverse</h1>
-      <canvas id="farmverse-game" width="800" height="600" />
+      <canvas id="farmverse-game" width="800" height="600"></canvas>
     </div>
   );
 }
